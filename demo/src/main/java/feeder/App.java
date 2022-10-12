d61d69e9ec9e10dd30b2f369d09b319a4119ee21
benjaminperez201831504@cunoc.edu.gt <mcdreck276@gmail.com>
2022-10-11 18:25:15 -0600

    Estructura de las carpetas

--- demo/src/main/java/feeder/App.java
+++ demo/src/main/java/feeder/App.java
@@ -1,14 +1,9 @@
 package feeder;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Las Flipantes Aventuras de Pedro el Mecanico");
+
     }
 }

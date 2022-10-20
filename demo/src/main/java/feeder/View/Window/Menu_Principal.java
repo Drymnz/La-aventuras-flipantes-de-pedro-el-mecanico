d5ecbc0bc9f524bab99e7726563cc4b2eb928e27
N3mmo <115595678+N3mmo@users.noreply.github.com>
2022-10-19 18:07:50 -0600

    Prueba
    Prueba de mover el sprite

--- demo/src/main/java/feeder/View/Window/Menu_Principal.java
+++ demo/src/main/java/feeder/View/Window/Menu_Principal.java
@@ -2,14 +2,20 @@ package feeder.View.Window;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class Menu_Principal extends JFrame implements ActionListener{
+public class Menu_Principal extends JFrame implements KeyListener{
     
     JButton botonIniciar;
     JPanel PanelIncial;
+    JLabel prueba;
     
     
     public Menu_Principal(){
@@ -19,36 +25,68 @@ public class Menu_Principal extends JFrame implements ActionListener{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
+        
         iniciarComponentes();
     
     
     }
-    
 
-    
 
     private void iniciarComponentes() {
+        
+        
         
         PanelIncial = new JPanel();
         PanelIncial.setBounds(0, 0, 1280, 720);
         PanelIncial.setVisible(true);
+        PanelIncial.setLayout(null);
         this.getContentPane().add(PanelIncial);
+        
+        prueba = new JLabel(new ImageIcon("auto.jpg"));
+        prueba.setBounds(100, 100, 50, 50);
         
         
         botonIniciar = new JButton("Empezar");
         botonIniciar.setBounds(400,200, 200,100);
-        PanelIncial.add(botonIniciar);
+       // PanelIncial.add(botonIniciar);
+        PanelIncial.addKeyListener(this);
+        
+        
+        
+       
+       PanelIncial. setFocusable(true);
+       PanelIncial. add(prueba);
         
         
     }
     
     
     
+    
+    
+    
+
     @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void keyTyped(KeyEvent e) {
+        
+        if (e.getKeyChar() == 'a') {
+            prueba.setLocation(prueba.getX()-5, prueba.getY());
+
+        }else if (e.getKeyChar() == 'd') {
+            prueba.setLocation(prueba.getX()+5, prueba.getY());
+
+        
+        }
     }
-    
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
     
     
     

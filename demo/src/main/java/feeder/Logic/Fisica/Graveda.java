3a3dcd28289339f59594b9ab337ab3e1468e1485
benjaminperez201831504@cunoc.edu.gt <mcdreck276@gmail.com>
2022-10-20 21:28:10 -0600

    iniciar con la graveda

---
+++ demo/src/main/java/feeder/Logic/Fisica/Graveda.java
@@ -0,0 +1,13 @@
+package feeder.Logic.Fisica;
+
+import feeder.Logic.Personajes.Personaje;
+
+public class Graveda {
+    private Personaje objeto;
+
+    public Graveda(Personaje objeto) {
+        this.objeto = objeto;
+    }
+    
+     
+}
